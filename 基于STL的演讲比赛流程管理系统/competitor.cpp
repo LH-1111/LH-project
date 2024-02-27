af61fd4e103c61a29c698040b23b668af75e8d56
Will <73372923+LH-1111@users.noreply.github.com>
2024-02-27 19:15:56 +0800

    Update competitor.cpp

--- 基于STL的演讲比赛流程管理系统/competitor.cpp
+++ 基于STL的演讲比赛流程管理系统/competitor.cpp
@@ -1,6 +1,6 @@
 #include "competitor.h"
 
-competitor::competitor(string name,float x[2]){
+competitor::competitor(string name,double x[2]){
     this->m_name = name;
     this->m_value[0] = x[0];
     this->m_value[1] = x[1];
